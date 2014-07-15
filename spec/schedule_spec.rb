0853d7523f22dea278f19b9d6fd8c1b3d85420f3
Connor Bode <connor.bode@gmail.com>
2014-07-14 20:11:39 -0400

    fix tests

--- spec/schedule_spec.rb
+++ spec/schedule_spec.rb
@@ -2,15 +2,16 @@ require_relative '../app/schedule.rb'
 require_relative '../app/error.rb'
 require 'rspec'
 require 'fakeweb'
+require 'tzinfo'
 
 describe 'schedule.rb' do
   describe 'getTime' do
     it 'returns the correct time' do
       s = Schedule.new "test", "test"
-      expect(s.getTime "11:45", 1, "Fall", 2014).to eq "2014-09-01T11:45:00-05:00"
-      expect(s.getTime "13:15", 5, "Fall", 2014).to eq "2014-09-05T13:15:00-05:00"
-      expect(s.getTime "13:15", 4, "Winter", 2015).to eq "2015-01-01T13:15:00-05:00"
-      expect(s.getTime "20:30", 3, "Winter", 2015).to eq "2015-01-07T20:30:00-05:00"
+      expect(s.getTime("11:45", 1, "Fall", 2014)[0..-6]).to eq "2014-09-01T11:45:00-"
+      expect(s.getTime("13:15", 5, "Fall", 2014)[0..-6]).to eq "2014-09-05T13:15:00-"
+      expect(s.getTime("13:15", 4, "Winter", 2015)[0..-6]).to eq "2015-01-01T13:15:00-"
+      expect(s.getTime("20:30", 3, "Winter", 2015)[0..-6]).to eq "2015-01-07T20:30:00-"
     end
   end
 
