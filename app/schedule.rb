feee748fab923863578326e084f63441f9b458e8
Connor Bode <connor.bode@gmail.com>
2014-07-14 20:08:37 -0400

    offset is now working

--- app/schedule.rb
+++ app/schedule.rb
@@ -115,9 +115,9 @@ class Schedule
   def getTime time, day, term, year
     case term
     when "Winter"
-      start_day = Time.new(year, 1, 1,)
+      start_day = Date.new(year, 1, 1,)
     when "Fall"
-      start_day = Time.new(year, 9, 1)
+      start_day = Date.new(year, 9, 1)
     # Need to figure out specifics for summer.
     # when "Summer"
     #   start_day = Date.new(year, 6, 1)
@@ -127,7 +127,7 @@ class Schedule
       start_day = start_day.next
     end
 
-    start_day.strftime("%Y-%m-%d") + "T#{time}:00-05:00"
+    start_day.to_time.strftime("%Y-%m-%d") + "T#{time}:00" + getOffset
   end
 
   def getOffset
