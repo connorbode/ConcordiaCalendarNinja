dc393b58664035e63b75786eb3036edcd3da1f8e
Connor Bode <connor.bode@gmail.com>
2014-08-19 20:05:29 -0400

    change api key

--- app/public/scripts/app.config.js
+++ app/public/scripts/app.config.js
@@ -3,7 +3,7 @@ angular.module('app.config', [])
   .constant('CONFIG', {
     "google": {
       "clientId": "375598335107-4uro6idndhosr9k1qas7v6f9fuhfpcsq.apps.googleusercontent.com",
-      "apiKey": "AIzaSyDh4-c9EcpqraodvPDlVoQEGCxq_YpQ8d4",
+      "apiKey": "AIzaSyCY4ZoGXyGZdccwnVTF9fqzUaPYhGj6WPg",
       "scopes": "https://www.googleapis.com/auth/calendar",
       "timezone": "America/Montreal"
     }
